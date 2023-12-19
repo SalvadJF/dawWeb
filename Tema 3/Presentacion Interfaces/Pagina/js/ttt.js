0f7f4800c84b666e4663738f92d7270bc1737333
Salvador Jimenez <salvad.j.f@gmail.com>
2023-12-19 17:22:07 +0100

    Presentcion de Interfaces

---
+++ Tema 3/Presentacion Interfaces/Pagina/js/ttt.js
@@ -0,0 +1,7 @@
+function abrirNav() {
+    document.getElementById("navegadorLateral").style.width = "250px";
+  }
+  
+function cerrarNav() {
+    document.getElementById("navegadorLateral").style.width = "0";
+  }
