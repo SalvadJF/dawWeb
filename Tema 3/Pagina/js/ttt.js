9bf4aaec39eff2ea492fce17c321fa1d598937aa
Salvador Jimenez <salvad.j.f@gmail.com>
2024-01-12 16:22:15 +0100

    12 de Enero , Interfaces , Acordeon creado

--- Tema 3/Pagina/js/ttt.js
+++ Tema 3/Pagina/js/ttt.js
@@ -5,3 +5,24 @@ function abrirNav() {
 function cerrarNav() {
     document.getElementById("navegadorLateral").style.width = "0";
   }
+
+const bloque = document.querySelectorAll('.bloque')
+const titulo = document.querySelectorAll('.tituloAco')
+
+// Cuando CLick en titulo
+  // Quitar la clase activo de todos los bloque
+  // Vamos a añadir la clase activo al bloque con la posicion del titulo
+
+  // Recorrer los titulos
+titulo.forEach(( cadaTitulo, i) => {
+  // Asigna el evento click
+  titulo[i].addEventListener('click', ()=> {
+    // Recorre los bloques
+    bloque.forEach( (cadaBloque, i)=> {
+      // Quita la clase activo de los bloques
+      bloque[i].classList.remove('activo')
+    })
+    // Añade la clase activo al bloque seleccionado
+    bloque[i].classList.add('activo')
+  })
+})
